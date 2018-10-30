b3f8a36345bf9a5797aa5ebfaec28ba24f5f0a1d
Suneel Marthi <smarthi@apache.org>
2018-10-30 07:53:33 -0400

    working output

--- cluster.py
+++ cluster.py
@@ -20,26 +20,32 @@ synonym_dict = {
    "prep" : "preparation",
    "certify" : "certification",
    "doc" : "document",
-   "trans" : "transaction"
+   "docs" : "document",
+   "documentation" : "document",
+   "trans" : "transaction",
+   "cert" : "certificate",
+   "amend" : "amendment",
+   "hoa" : "homeowners association"
 }
 
 stemmer = SnowballStemmer("english")
 
 def tokenize(line):
 
-    line = re.sub("[^a-zA-Z ]", " ", line).strip()
+    line = re.sub("[^a-zA-Z ]", " ", line).strip().lower()
 
     tokens = []
     for token in nltk.word_tokenize(line):
 
-        if not token.lower() in stopwords and len(token) > 1:
-            if synonym_dict.keys().__contains__(token.lower()):
-                token = synonym_dict.get(token.lower())
+        if not token in stopwords and len(token) > 1:
+            if token in synonym_dict.keys():
+                token = synonym_dict.get(token)
 
-            tokens.append(stemmer.stem(token))
+            for tok in nltk.word_tokenize(token):
+                tokens.append(stemmer.stem(tok))
 
     if len(tokens) == 0:
-        tokens.append("__ALL_TOKENS_REPLACED__")
+        tokens.append("other")
 
     return tokens
 
@@ -48,11 +54,12 @@ def main():
     with open('raw_fee_fields.txt', 'r', encoding="utf-8") as code_file:
         fields = code_file.read().splitlines()
 
-    vectorizer = TfidfVectorizer(use_idf=True, tokenizer=tokenize, norm="l2", ngram_range=(1,3), analyzer='word')
+    vectorizer = TfidfVectorizer(use_idf=False, tokenizer=tokenize, norm="l2",  analyzer='word', sublinear_tf=True,
+                                 ngram_range=(1,3))
 
     doc_matrix = vectorizer.fit_transform(fields)
 
-    kmeans = KMeans(init='k-means++', n_clusters=30, n_init=10)
+    kmeans = KMeans(init=doc_matrix[-144:].toarray(), n_clusters=144, n_init=10)
 
     kmeans.fit(doc_matrix)
 
